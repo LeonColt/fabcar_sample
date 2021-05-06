30ec6048ab6159c4bfd94bcac018e8e52aa60462
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-05-06 14:47:39 +0700

    listen on port according to .env file build swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get( ConfigService );
+
+  const documentationOptions = new DocumentBuilder()
+        .setTitle("FAB CAR")
+        .setDescription("FAB CAR Backend Documentation")
+        .setVersion("0.0.1")
+        .setContact( 'LC', null, 'riyanto.1230.2828@gmail.com' )
+        .addBearerAuth({
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+            in: 'header'
+        } )
+        .build();
+
+    const documentation = SwaggerModule.createDocument( app, documentationOptions );
+    SwaggerModule.setup( "docs", app, documentation );
+
+  await app.listen(+configService.get('PORT'));
 }
 bootstrap();
