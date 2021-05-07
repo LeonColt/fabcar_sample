87549f41c4e30dba7ee858c8de568b0308fc97f3
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-05-07 13:11:57 +0700

    add transferCar and find one model

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { ApiConflictResponse, ApiCreatedResponse, ApiForbiddenResponse, ApiNoContentResponse, ApiOkResponse, ApiOperation, ApiUnauthorizedResponse } from '@nestjs/swagger';
-import { CreateCar, Username } from './app.model';
+import { CarTransfer, CreateCar, Username } from './app.model';
 import { AppService } from './app.service';
 
 @Controller("api")
@@ -10,7 +10,7 @@ export class AppController {
   @ApiOperation({ summary: "Register Admin" })
   @ApiNoContentResponse({ description: "Register admin success" })
   @ApiConflictResponse({ description: "Admin already registered" })
-  @HttpCode( HttpStatus.CREATED )
+  @HttpCode( HttpStatus.NO_CONTENT )
   @Post("register/admin")
   async registerAdmin() {
     await this.appService.registerAdmin();
@@ -20,7 +20,7 @@ export class AppController {
   @ApiNoContentResponse({ description: "Register user success" })
   @ApiConflictResponse({ description: "User already registered" })
   @ApiForbiddenResponse({ description: "You must register admin first" })
-  @HttpCode( HttpStatus.CREATED )
+  @HttpCode( HttpStatus.NO_CONTENT )
   @Post("register")
   async registerUser( @Body() username: Username ) {
     await this.appService.register(username.username);
@@ -34,12 +34,27 @@ export class AppController {
     return await this.appService.createCar( username, body );
   }
 
-  @ApiOperation({ summary: "Query ledger" })
-  @ApiOkResponse({ description: "Query Result" })
-  @ApiUnauthorizedResponse({ description: "Unauthorized access, please register your username" })
-  @Get(":username")
-  async query( @Param("username") username : string ) {
-    return await this.appService.query(username);
+  @ApiOperation({ summary: "Transfer car ownership" })
+  @ApiNoContentResponse({ description: "Transfer ownership success" })
+  @ApiUnauthorizedResponse({ description: "Please register user first" })
+  @Put( "transfer/:username" )
+  async transferCar( @Param("username") username: string, @Body() body: CarTransfer ) : Promise<void> {
+    await this.appService.transfer( username, body );
   }
 
+  @ApiOperation({ summary: "Find One Car" })
+  @ApiOkResponse({ description: "A Car" })
+  @ApiUnauthorizedResponse({ description: "Unauthorized access, please register your username" })
+  @Get(":username/car/:number")
+  async findOne( @Param("username") username: string, @Param("number") carNumber: string ) {
+    return await this.appService.findOne( username, carNumber );
+  }
+
+  @ApiOperation({ summary: "Find Car List" })
+  @ApiOkResponse({ description: "Car List" })
+  @ApiUnauthorizedResponse({ description: "Unauthorized access, please register your username" })
+  @Get(":username")
+  async find( @Param("username") username : string ) {
+    return await this.appService.find(username);
+  }
 }
